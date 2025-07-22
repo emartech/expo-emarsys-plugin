00a595de2d7601f26ca68dd84ffc876b79099d35
Eduardo Zatoni <45048607+eduzatoni@users.noreply.github.com>
2025-07-22 18:29:48 +0200

    feat: initialise Emarsys SDK for iOS

--- ios/AppDelegateSubscriber.swift
+++ ios/AppDelegateSubscriber.swift
@@ -7,9 +7,9 @@ public class AppDelegateSubscriber: ExpoAppDelegateSubscriber {
   public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 
     let config = EMSConfig.make { builder in
-          builder.setMobileEngageApplicationCode("EMSE7-A1D58")
-          builder.setMerchantId("XXXXXXXXXXXXX")
-      }
+      builder.setMobileEngageApplicationCode("EMSE7-A1D58")
+      builder.setMerchantId("XXXXXXXXXXXXX")
+    }
     Emarsys.setup(config: config)
     UNUserNotificationCenter.current().delegate = Emarsys.push
     // TODO: handle emarsys events

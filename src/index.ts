de742c362a499b73b38db02197635151444c9b30
Eduardo Zatoni <45048607+eduzatoni@users.noreply.github.com>
2025-07-24 13:38:29 +0200

    chore: Refactor plugin to config plugin structure

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-// Reexport the native module. On web, it will be resolved to ExpoEmarsysPluginModule.web.ts
-// and on native platforms to ExpoEmarsysPluginModule.ts
-export { default } from './ExpoEmarsysPluginModule';
-export { default as ExpoEmarsysPluginView } from './ExpoEmarsysPluginView';
-export * from  './ExpoEmarsysPlugin.types';
---
+++ src/withEmarsysPlugin.ts
@@ -0,0 +1,25 @@
+import { ConfigPlugin, createRunOncePlugin } from "expo/config-plugins";
+// import withAndroidPlugin from "./withAndroidPlugin";
+// import withIosPlugin from "./withIosPlugin";
+
+const withEmarsysPlugin: ConfigPlugin<{
+  applicationCode: string,
+  merchantId: string
+}> = (
+  config,
+  options
+) => {
+  console.log("withEmarsysPlugin called with options:", options);
+  // config = withAndroidPlugin(config, options);
+  // Then apply iOS modifications and return
+  // return withIosPlugin(config, options);
+  return config;
+};
+
+import pkg from "../package.json";
+
+export default createRunOncePlugin(
+  withEmarsysPlugin,
+  pkg.name,
+  pkg.version
+);
